86dcb10c2198a3cad46beebaddc5b79b111e97e6
haeny-dev <wjdeoaks92@gmail.com>
2023-05-06 19:52:44 +0900

    Part3.스프링 부트 스타트

---
+++ fastcampus-springboot/src/main/kotlin/com/fastcampus/fastcampusspringboot/FastcampusSpringbootApplication.kt
@@ -0,0 +1,11 @@
+package com.fastcampus.fastcampusspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastcampusSpringbootApplication
+
+fun main(args: Array<String>) {
+    runApplication<FastcampusSpringbootApplication>(*args)
+}
---
+++ fastcampus-springboot/src/test/kotlin/com/fastcampus/fastcampusspringboot/FastcampusSpringbootApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.fastcampus.fastcampusspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastcampusSpringbootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
