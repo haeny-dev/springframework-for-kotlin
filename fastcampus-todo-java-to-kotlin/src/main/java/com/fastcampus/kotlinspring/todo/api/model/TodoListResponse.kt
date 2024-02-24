6f9689e1079e3fb113e1202b4b00a60b8369cded
haeny-dev <wjdeoaks92@gmail.com>
2024-02-24 17:42:28 +0900

    Part4 > Ch02 > 03. 컨트롤러 레이어 리팩토링

---
+++ fastcampus-todo-java-to-kotlin/src/main/java/com/fastcampus/kotlinspring/todo/api/model/TodoListResponse.kt
@@ -0,0 +1,20 @@
+package com.fastcampus.kotlinspring.todo.api.model
+
+import com.fastcampus.kotlinspring.todo.domain.Todo
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+data class TodoListResponse(
+    val items: List<TodoResponse>,
+) {
+    val size: Int
+        @JsonIgnore
+        get() = items.size
+
+    fun get(index: Int): TodoResponse = items[index]
+
+    companion object {
+        fun of(todoList: List<Todo>) = TodoListResponse(
+            items = todoList.map(TodoResponse::of)
+        )
+    }
+}
