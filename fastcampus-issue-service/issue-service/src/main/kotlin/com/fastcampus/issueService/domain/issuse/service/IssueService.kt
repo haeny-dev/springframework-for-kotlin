d6df291f62326868e23e0ff466087a4959848d5f
haeny-dev <wjdeoaks92@gmail.com>
2024-03-09 19:05:52 +0900

    Part5 > Ch01 > 08. 이슈 수정

--- fastcampus-issue-service/issue-service/src/main/kotlin/com/fastcampus/issueService/domain/issuse/service/IssueService.kt
+++ fastcampus-issue-service/issue-service/src/main/kotlin/com/fastcampus/issueService/domain/issuse/service/IssueService.kt
@@ -6,6 +6,7 @@ import com.fastcampus.issueService.domain.issuse.model.IssueRequest
 import com.fastcampus.issueService.domain.issuse.model.IssueResponse
 import com.fastcampus.issueService.domain.issuse.repository.IssueRepository
 import com.fastcampus.issueService.global.exception.NotFoundException
+import org.aspectj.weaver.ast.Not
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -41,5 +42,24 @@ class IssueService(
     fun get(id: Long): IssueResponse {
         val issue = issueRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈가 존재하지 않습니다.")
         return IssueResponse(issue)
+    }
+
+    @Transactional
+    fun edit(
+        userId: Long,
+        id: Long,
+        request: IssueRequest
+    ): IssueResponse {
+        val issue = issueRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈가 존재하지 않습니다.")
+
+        return with(issue) {
+            summary = request.summary
+            description = request.description
+            this.userId = userId
+            type = request.type
+            priority = request.priority
+            status = request.status
+            IssueResponse(issueRepository.save(this))
+        }
     }
 }
