8b0fababc1f9e48268402ca77da753eae5e2ea05
haeny-dev <wjdeoaks92@gmail.com>
2024-03-11 06:41:23 +0900

    Part6 > Ch02 > 03. 함수형 엔드포인트

--- fastcampus-spring-webflux-practice/src/main/kotlin/com/fastcampus/springwebflux/Router.kt
+++ fastcampus-spring-webflux-practice/src/main/kotlin/com/fastcampus/springwebflux/Router.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.RouterFunctions.route
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
 
 @Configuration
 class Router {
@@ -13,5 +14,17 @@ class Router {
     fun helloRouter(handler: HelloHandler): RouterFunction<ServerResponse> =
         route()
             .GET("/", handler::sayHello)
+            .POST("/", handler::sayHello)
+            .DELETE("/", handler::sayHello)
             .build()
+
+    @Bean
+    fun userRouter(handler: UserHandler): RouterFunction<ServerResponse> =
+        router {
+            "/users".nest {
+                GET("/{id}", handler::getUser)
+                GET("", handler::getAll)
+            }
+        }
+
 }
