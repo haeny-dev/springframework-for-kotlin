67d9d9dda7e92113ce27a34f5c9d553c288fcbd0
haeny-dev <wjdeoaks92@gmail.com>
2024-06-05 22:15:16 +0900

    lecture: 공변과 반공변

--- inflearn-kotlin-advanced/src/main/kotlin/com/inflearn/kotlin/advanced/lec02/Cage2.kt
+++ inflearn-kotlin-advanced/src/main/kotlin/com/inflearn/kotlin/advanced/lec02/Cage2.kt
@@ -13,8 +13,21 @@ class Cage2<T> {
         this.animals.add(animal)
     }
 
-    fun moveFrom(otherCage: Cage2<T>) {
+    // out: variance(변성) annotation
+    // 데이터를 꺼낼 수만 있다.
+    // 생산자 역할만 한다.
+    // 타입안정성이 깨지기 때문에..
+    fun moveFrom(otherCage: Cage2<out T>) {
+        otherCage.getFirst()
+//        otherCage.put(Carp("잉어")) // Error
         this.animals.addAll(otherCage.animals)
+    }
+
+    // 반공변(contra-variance) 하게 만들자
+    // 함수 파마리터 입장에서의
+    // 소비자, 반공변
+    fun moveTo(otherCage: Cage2<in T>) {
+        otherCage.animals.addAll(this.animals)
     }
 }
 
@@ -23,5 +36,7 @@ fun main() {
     goldFishCage.put(GoldFish("금붕어"))
 
     val fishCage = Cage2<Fish>()
-//    fishCage.moveFrom(goldFishCage)   // Error
+    fishCage.moveFrom(goldFishCage)
+
+    val fish: Fish = fishCage.getFirst()
 }
