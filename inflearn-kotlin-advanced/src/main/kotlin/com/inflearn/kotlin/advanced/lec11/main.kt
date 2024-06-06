baa9062ba22927b174d01c381f7f1cc1af6e288c
haeny-dev <wjdeoaks92@gmail.com>
2024-06-06 16:45:41 +0900

    lecture: 코틀린 고급편 - 위임과 관련된 몇 가지 추가 기능

---
+++ inflearn-kotlin-advanced/src/main/kotlin/com/inflearn/kotlin/advanced/lec11/main.kt
@@ -0,0 +1,62 @@
+package com.inflearn.kotlin.advanced.lec11
+
+import com.inflearn.kotlin.advanced.lec08.Person
+import com.inflearn.kotlin.advanced.lec09.Person3
+import kotlin.properties.PropertyDelegateProvider
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+fun main() {
+//    val status: String by object : ReadOnlyProperty<Person3, String> {
+//        private var isGreen: Boolean = false
+//
+//        override fun getValue(thisRef: Person3, property: KProperty<*>): String {
+//            return if (isGreen) {
+//                "Happy"
+//            } else {
+//                "Sad"
+//            }
+//        }
+//    }
+    Person5()
+}
+
+class Person5 {
+    val name by DelegateProvider("정주현") // 정상동작
+    val country by DelegateProvider("한국") // 정상동작 X
+}
+
+class DelegateProvider(
+    private val initValue: String,
+) : PropertyDelegateProvider<Any, DelegateProperty> {
+    override fun provideDelegate(thisRef: Any, property: KProperty<*>): DelegateProperty {
+        if (property.name != "name") {
+            throw IllegalArgumentException("name 만 연결 가능합니다. property name: ${property.name}")
+        }
+
+        return DelegateProperty(initValue)
+    }
+}
+
+class DelegateProperty(
+    private val initValue: String,
+) : ReadOnlyProperty<Any, String> {
+    override fun getValue(thisRef: Any, property: KProperty<*>): String {
+        return initValue
+    }
+}
+
+class LazyInitProperty<T>(val init: () -> T): ReadOnlyProperty<Any, T> {
+    private var _value: T? = null
+    val value: T
+        get() {
+            if (_value == null) {
+                this._value = init()
+            }
+            return _value!!
+        }
+
+    override fun getValue(thisRef: Any, property: KProperty<*>): T {
+        TODO("Not yet implemented")
+    }
+}
